11a914f07203aa625e8ad444ae8f88bc1888ee1d
JonasFocke01 <48436748+JonasFocke01@users.noreply.github.com>
2023-08-16 09:15:27 +0200

    Update linux_installer.sh

--- linux_installer.sh
+++ linux_installer.sh
@@ -144,10 +144,19 @@ if [ $confirmation = "y" ]; then
 	sudo apt install pavucontrol -y
 fi
 
-echo "Install node with nvm? (y/n)"
+echo "Install nvm? (y/n)"
 read confirmation;
 if [ $confirmation = "y" ]; then
 	wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
+fi
+
+echo "Install spotify? (y/n)"
+read confirmation;
+if [ $confirmation = "y" ]; then
+	curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+	echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+ 	sudo apt update
+	sudo apt install spotify -y
 fi
 
 echo "Perform cleanup? (y/n)"
