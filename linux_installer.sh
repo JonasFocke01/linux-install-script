d435e2fb6c04ba2f1ddef70d6fa1ea664d872727
JonasFocke01 <48436748+JonasFocke01@users.noreply.github.com>
2023-10-04 16:59:16 +0200

    Install new deps for remindy

--- linux_installer.sh
+++ linux_installer.sh
@@ -141,6 +141,8 @@ if [ $confirmation = "y" ]; then
 	sudo apt install libssl-dev -y
 	sudo apt install cmake -y
  	sudo apt install gtk-3-dev -y
+  	sudo apt install libasound2-dev -y
+   	sudo apt install libwebkit2gtk-4.0 -y
 	cargo-deb
 	cd ../../..
 	sudo dpkg -i Desktop/remindy/remindy/target/debian/remindy_0.1.0_amd64.deb
